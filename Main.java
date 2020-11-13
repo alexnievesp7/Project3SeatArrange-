88a070c9fbea40ae51692e952b57c2c896f00a9b
alexnievesp7 <alejandro.nieves@patelhs.org>
2020-11-13 01:46:46 +0000

    Final

--- Main.java
+++ Main.java
@@ -6,12 +6,15 @@ class Main {
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
     String[] names = new String[15];
     Scanner scan = new Scanner(System.in);
+    
     for (int i = 0; i < 15; i++) {
       System.out.println("What is your name?");
       names[i] = scan.next();
-    
-     for(int i = 0; i < 15; i++)
-      System.out.println(seats[i]);
+      }
+      int i = 0;
+      while (i<15) {
+      System.out.println(names[i] + seats[i]);
+      i++;
 
     }
   }
